53eba6e97c8457afd1a715b55f1cbb54c39c23a1
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-14 15:14:10 -0300

    Style Background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h2>Jogo da Forca</h2>
     </div>
   );
 }
---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Capô", "Antena", "Roda", "Pneu", "Parabrisa"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate", "Goiaba"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Boca", "Nariz", "Cabelo"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "HeadSet", "Microfone", "Processador", "Placa de Video"],
+    programação: ["JavaScript", "Python", "Linguagem", "Framework", "React", "Angular", "Vue", "Node", "c#", "MongoDB"],
+    alimento: ["Arroz", "Feijão", "Batata", "Aveia", "Frango", "Ovo", "Leite", "Manteiga"]
+};

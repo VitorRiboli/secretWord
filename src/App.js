8ee226c658364b60ed44062ffe88eaacf9134131
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-15 20:00:41 -0300

    feat

--- src/App.js
+++ src/App.js
@@ -22,8 +22,39 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState("")
+
+  const pickWordAndPickCategory = () => {
+    // pick randon category
+    const categories = Object.keys(words) //pegando as categorias dentro do objeto
+    //Pegando uma categoria aleatoria, usei o floor para retornar um numero inteiro, random para ser aleatorio, object.keys para selecionar o local, lenght para percorrer todos.
+    const category = categories[(Math.floor(Math.random() * Object.keys(categories).length))]
+  
+    //pick randon word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { word, category }  //entre chaves para retornar como array, se for colchetes [], sretornará objetos
+  }
+
   // Start Game
   const startGame = () => {
+    //pick word and pick category
+    const { word, category } = pickWordAndPickCategory();
+    
+    //Create an array of letters
+    let wordLetters = word.toLowerCase().split(""); //split retorna as letras separadas, vai retornar uma lista
+    
+    console.log(category, word);
+    console.log(wordLetters);
+
+    // Fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    // Next Game Stage
     setGameStage(stages[1].name);
   }
 

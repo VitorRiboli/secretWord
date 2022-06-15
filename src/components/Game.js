883df49782309f59bb6f5eba717217d8bdb965c8
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-15 20:28:04 -0300

    Style: game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,43 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({ verifyLetter }) => {
   return (
-    <div>
-        <h1>game</h1>
-        <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className='game'>
+
+      <div className="headerContainer">
+        <h1>Advinhe a Palavra</h1>
+        <p className='points'>
+          <span>Pontuação: 000</span>
+        </p>
+      </div>
+
+      <h3 className='tip'>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className='wordContainer'>
+        <span className='letter'></span>
+        <span className="blankSquare"></span>
+      </div>
+
+      <div className='letterContainer'>
+        <p>Tente advinhar uma letra da palavra</p>
+        <form>
+          <input type="text" name='letter'
+            maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <div className='span'>
+          <span>a,</span>
+          <span>b,</span>
+          <span>c,</span>
+        </div>
+      </div>
+      
     </div>
   )
 }
